b1f4ca8b9fa1f7cb60951bd8ccaa3ea015a554bb
taissa-rodrigues <taissavrp@gmail.com>
2023-09-16 14:53:54 -0300

    Initial Commit

---
+++ Memorize-App.3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize-App.3
+//
+//  Created by user on 16/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize-App.3/Memorize_App_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_App_3App.swift
+//  Memorize-App.3
+//
+//  Created by user on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_App_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
